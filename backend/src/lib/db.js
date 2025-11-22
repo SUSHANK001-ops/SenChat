a3929f71e6205759baae19e304e759959d22c272
Sushank <sushanklamichhane12@gmail.com>
2025-11-22 18:50:51 +0545

    refactor signup process and improve token generation error handling

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
-dotenv.config()
-const ConnectDB = async() =>{
-           try {
-            const conn = await mongoose.connect(process.env.MONGO_URI)
-            console.log("Database Connected succesfully" , conn.connection.host)
-           } catch (error) {
-            console.error("Error connection to DATABASE" , error)
-            process.exit(1); //1 status means fail , 0 means success
-           } 
-}
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const ConnectDB = async () => {
+  try {
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is missing!!!!");
+    }
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connected succesfully", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to DATABASE", error);
+    process.exit(1); //1 status means fail , 0 means success
+  }
+};
 
 export default ConnectDB;
